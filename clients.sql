6b52098957874442e554d3d467c248d49f9497e0
marcdancing <marcollepastor@gmail.com>
2024-01-22 16:27:31 +0100

    Script crear taules client

---
+++ clients.sql
@@ -0,0 +1,10 @@
+Create table clients(
+    id Number(8),
+    nom varchar(32),
+    cognom1 varchar(75),
+    cognom2 varchar(75),
+    usuari varchar(50),
+    email varchar(50),
+    clau varchar(64),
+    tipus int(2)
+); 
